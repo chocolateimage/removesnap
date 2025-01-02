1803739ccf82d37b6c28f38a1e34cdb4126dab14
chocolateimage <chocolateimage@protonmail.com>
2025-01-02 15:34:17 +0100

    Update Firefox installation to Mozilla's official way

--- removesnap.sh
+++ removesnap.sh
@@ -12,24 +12,30 @@ fi
 
 # === Firefox ===
 # Based on https://askubuntu.com/a/1404401
+# and https://support.mozilla.org/en-US/kb/install-firefox-linux#w_install-firefox-deb-package-for-debian-based-distributions-recommended
 
 killall firefox
 
 mv -f ~/snap/firefox/common/.mozilla ~/
 
+# For Thunderbird
 sudo add-apt-repository --yes ppa:mozillateam/ppa
 
-echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
-
+# Setup APT repo
+sudo install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
 echo '
 Package: *
-Pin: release o=LP-PPA-mozillateam
-Pin-Priority: 1001
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
 
-Package: firefox
-Pin: version 1:1snap*
+Package: thunderbird
+Pin: version 2:1snap*
 Pin-Priority: -1
-' | sudo tee /etc/apt/preferences.d/mozilla-firefox
+' | sudo tee /etc/apt/preferences.d/mozilla
+
+sudo apt update
 
 sudo snap remove firefox
 
