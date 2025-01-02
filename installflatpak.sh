35f2710a088efa14f1c81b5570b4d8090f1de27b
chocolateimage <chocolateimage@protonmail.com>
2025-01-02 15:54:11 +0100

    Use kdialog if present on restart dialog

--- installflatpak.sh
+++ installflatpak.sh
@@ -15,8 +15,14 @@ if dpkg -s gnome-software; then
     sudo apt install -y gnome-software-plugin-flatpak
 fi
 
-sudo apt install -y zenity
+if command -v kdialog; then
+    if kdialog --yesno "A restart is required, do you want to restart now?" --title "Flatpak install"; then
+        reboot
+    fi
+else
+    sudo apt install -y zenity
 
-if zenity --question --title="Flatpak install" --text="A restart is required, do you want to restart now?" --ok-label=Yes --cancel-label=No; then
-    reboot
+    if zenity --question --title="Flatpak install" --text="A restart is required, do you want to restart now?" --ok-label=Yes --cancel-label=No; then
+        reboot
+    fi
 fi
